c84698e98f1afed45fd3f41ebf4045165b668bcd
HasanMufdy <96997047+HasanMufdy@users.noreply.github.com>
2023-12-09 19:47:50 +0300

    Database is added

---
+++ HR-System/HR-System/Migrations/20231209164235_test.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Salaries_Employees_EmployeeId",
+                table: "Salaries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Salaries_EmployeeId",
+                table: "Salaries");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "Salaries");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SalaryId",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_SalaryId",
+                table: "Employees",
+                column: "SalaryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Salaries_SalaryId",
+                table: "Employees",
+                column: "SalaryId",
+                principalTable: "Salaries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Salaries_SalaryId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_SalaryId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "SalaryId",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeId",
+                table: "Salaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Salaries_EmployeeId",
+                table: "Salaries",
+                column: "EmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Salaries_Employees_EmployeeId",
+                table: "Salaries",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ HR-System/HR-System/Models/Salary.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HR_System.Models
+{
+    public class Salary
+    {
+        public int Id { get; set; }
+
+        //public int EmployeeId { get; set; }
+        public int Amount { get; set; }
+        public string Currency { get; set; }
+        public DateTime EffectiveDate { get; set; }
+
+        //public Employee Employee { get; set; }
+    }
+}
